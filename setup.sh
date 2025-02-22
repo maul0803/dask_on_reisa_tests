f53636626e3dae673be8754d2b9260342a1dcfff
maul0803 <quintyn.c@hotmail.com>
2025-02-22 16:45:49 +0100

    F

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+module load cesga/2020 python/3.9.9
+module load cesga/2022 gcc/system ucx/1.11.2 gdrcopy/2.3 slurm/cesga gcccore/system hwloc/2.7.1 cuda/system ucx-cuda/1.11.2 openmpi/4.1.4
+export PATH=$PATH:/home/ulc/cursos/curso341/.local/bin
